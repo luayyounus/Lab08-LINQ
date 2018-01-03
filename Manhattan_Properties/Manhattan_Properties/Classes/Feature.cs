ddab32e9718ab9858431adfdfc7b53fa37011803
Luay <luayghazwan@yahoo.com>
2018-01-03 11:23:25 -0800

    Feature Class

---
+++ Manhattan_Properties/Manhattan_Properties/Classes/Feature.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manhattan_Properties.Classes
+{
+    class Feature
+    {
+        public string Type { get; set; }
+        public Geometry Geometry { get; set; }
+        public Properties Properties { get; set; }
+    }
+}
