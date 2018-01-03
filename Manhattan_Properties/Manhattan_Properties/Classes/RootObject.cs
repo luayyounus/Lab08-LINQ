b0e36101385e10d1e5e6d821bce2a81450819bc2
Luay <luayghazwan@yahoo.com>
2018-01-03 11:24:02 -0800

    Root Object Class Created

---
+++ Manhattan_Properties/Manhattan_Properties/Classes/RootObject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manhattan_Properties.Classes
+{
+    class RootObject
+    {
+        public string Type { get; set; }
+        public List<Feature> Features { get; set; }
+    }
+}
